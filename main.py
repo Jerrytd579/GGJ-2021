85c0e7250b24368020d9340343a5286b412fab5e
leo ouyang <Leo Ouyang>
2021-01-29 15:59:04 -0800

    Wall detection added

--- main.py
+++ main.py
@@ -14,46 +14,60 @@ black = (0,0,0)
 white = (255,255,255) 
 
 clock = pygame.time.Clock()
-crashed = False
+crashed = False      
 
-class Rect:
-    def __init__(self,x,y,z,a):
-        self.x = x
-        self.y = y
-        self.z = z;
-        self.a = a
-       
-
-        
+class Level:
+    walls = []
+    def addWall(self,wall):
+        self.walls.append(wall)
+    def update(self):
+        for i in self.walls:
+            pygame.draw.rect(gameDisplay,black,i)
 
 class Dude:
     def __init__(self, rect):
         self.rect = rect
-        self.speed = pygame.Vector2(0,0)
-        self.velocity = 1
-        self.accel = pygame.Vector2(0,0)
-        self.angle = 90
         self.img = pygame.image.load('sprites/blastRocket.png')
-    def update(self):
+    def update(self,level):
         keys = pygame.key.get_pressed();
-        velocity = 1
-        if (keys[pygame.K_SPACE]):
-            velocity = 10
-        #velocity *= pygame.time.Clock.get_time()
+        velocity = .25
+        if (keys[pygame.K_LSHIFT]):
+            velocity = .5
+        velocity *= clock.get_time()
+        speeds = pygame.Vector2(0,0)
         if (keys[pygame.K_RIGHT]):
-            self.rect.x += velocity
+            speeds[0] += velocity
         elif keys[pygame.K_LEFT]:
-            self.rect.x -= velocity
+            speeds[0] -= velocity
         if keys[pygame.K_UP]:
-            self.rect.y -= velocity
+            speeds[1] -= velocity
         elif keys[pygame.K_DOWN]:
-            self.rect.y += velocity
-        render(self.img,self.rect,self.angle)
+            speeds[1] += velocity
+        render(self.img,self.rect,0)
+        horizRect = self.rect.move(speeds[0],0)
+        vertRect = self.rect.move(0,speeds[1])
+        collidedHoriz = True
+        collidedVert = True
+        for i in level.walls:
+            if i.colliderect(horizRect):
+                collidedHoriz = False
+            if i.colliderect(vertRect):
+                collidedVert = False;
+        if collidedHoriz:
+            self.rect.x = horizRect.x
+        if collidedVert:
+            self.rect.y = vertRect.y
+                
 def render(img,rect,angle):
-    gameDisplay.blit(pygame.transform.rotate(pygame.transform.scale(img,(rect.z,rect.a)),angle), (rect.x,rect.y))
+    gameDisplay.blit(pygame.transform.rotate(pygame.transform.scale(img,(rect.w,rect.h)),angle), (rect.x,rect.y))
 
-r = Dude(Rect(display_width*.5,display_height - 64, 64,64))
-camera = Rect(0,0,display_width,display_height)
+r = Dude(pygame.Rect(display_width*.5,display_height - 64, 64,64))
+l = Level()
+l.addWall(pygame.Rect(10,10,64,64))
+l.addWall(pygame.Rect(100,100,64,64))
+l.addWall(pygame.Rect(200,200,100,64))
+camera = pygame.Rect(0,0,display_width,display_height)
+clock = pygame.time.Clock()
 while not crashed:
 
     for event in pygame.event.get():
@@ -61,9 +75,10 @@ while not crashed:
             crashed = True
 
     gameDisplay.fill(white)
-    r.update()
-    camera.x = r.rect.x + r.rect.z/2 - camera.z/2
-    camera.y = r.rect.y + r.rect.a/2 - camera.a/2
+    r.update(l)
+    l.update()
+    camera.x = r.rect.x + r.rect.w/2 - camera.w/2
+    camera.y = r.rect.y + r.rect.h/2 - camera.h/2
         
     pygame.display.update()
     clock.tick(60)
